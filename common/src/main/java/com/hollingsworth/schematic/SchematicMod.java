da851de508111033f6a5fdff2e3c838be83903f3
Bailey Hollingsworth <baileyholl@gmail.com>
2023-09-02 20:57:31 -0500

    add modid

--- common/src/main/java/com/hollingsworth/schematic/SchematicMod.java
+++ common/src/main/java/com/hollingsworth/schematic/SchematicMod.java
@@ -1,17 +1,9 @@
 package com.hollingsworth.schematic;
 
 import com.hollingsworth.schematic.platform.Services;
-import net.minecraft.core.Registry;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.TooltipFlag;
-
-import java.util.List;
 
 public class SchematicMod {
-
+    public static final String MODID = "schematic";
     // This method serves as an initialization hook for the mod.
     public static void init() {
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.isDevelopmentEnvironment() ? "development" : "production");
