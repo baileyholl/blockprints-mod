ea3e460f85312230094095f2589b5d07517d916e
Bailey Hollingsworth <baileyholl@gmail.com>
2024-02-19 22:24:01 -0600

    wip structure renderer

---
+++ common/src/main/java/com/hollingsworth/schematic/client/renderer/DireRenderMethods.java
@@ -0,0 +1,201 @@
+package com.hollingsworth.schematic.client.renderer;
+
+import com.google.common.collect.ImmutableMap;
+import com.hollingsworth.schematic.common.util.Color;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+
+import java.util.Map;
+
+import static net.minecraft.client.renderer.RenderType.entityTranslucentCull;
+
+public class DireRenderMethods {
+    private static Map<BlockEntityType<?>, BlockEntityRenderer<?>> renderers = ImmutableMap.of();
+
+    public static void renderBETransparent(BlockState pState, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay, float alpha) {
+        MultiplyAlphaRenderTypeBuffer multiplyAlphaRenderTypeBuffer = new MultiplyAlphaRenderTypeBuffer(pBufferSource, alpha);
+        ItemStack stack = new ItemStack(pState.getBlock());
+        net.minecraftforge.client.extensions.common.IClientItemExtensions.of(stack).getCustomRenderer().renderByItem(stack, ItemDisplayContext.NONE, pPoseStack, multiplyAlphaRenderTypeBuffer, pPackedLight, pPackedOverlay);
+    }
+
+    public static class MultiplyAlphaRenderTypeBuffer implements MultiBufferSource {
+        private final MultiBufferSource inner;
+        private final float constantAlpha;
+
+        public MultiplyAlphaRenderTypeBuffer(MultiBufferSource inner, float constantAlpha) {
+            this.inner = inner;
+            this.constantAlpha = constantAlpha;
+        }
+
+        @Override
+        public VertexConsumer getBuffer(RenderType type) {
+            RenderType localType = type;
+            if (localType instanceof RenderType.CompositeRenderType) {
+                // all of this requires a lot of AT's so be aware of that on ports
+                ResourceLocation texture = ((RenderStateShard.TextureStateShard) ((RenderType.CompositeRenderType) localType).state.textureState).texture
+                        .orElse(InventoryMenu.BLOCK_ATLAS);
+
+                localType = entityTranslucentCull(texture);
+            } else if (localType.toString().equals(Sheets.translucentCullBlockSheet().toString())) {
+                localType = Sheets.translucentCullBlockSheet();
+            }
+
+            return new DireVertexConsumer(this.inner.getBuffer(localType), constantAlpha);
+        }
+    }
+
+    public static void renderBESquished(BlockState pState, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay, float alpha) {
+        SquishedRenderTypeBuffer multiplyAlphaRenderTypeBuffer = new SquishedRenderTypeBuffer(pBufferSource, alpha, pPoseStack.last().pose());
+        ItemStack stack = new ItemStack(pState.getBlock());
+        net.minecraftforge.client.extensions.common.IClientItemExtensions.of(stack).getCustomRenderer().renderByItem(stack, ItemDisplayContext.NONE, pPoseStack, multiplyAlphaRenderTypeBuffer, pPackedLight, pPackedOverlay);
+    }
+
+    public static class SquishedRenderTypeBuffer implements MultiBufferSource {
+        private final MultiBufferSource inner;
+        private final float squishAmt;
+        private final Matrix4f matrix4f;
+
+        public SquishedRenderTypeBuffer(MultiBufferSource inner, float squishAmt, Matrix4f matrix4f) {
+            this.inner = inner;
+            this.squishAmt = squishAmt;
+            this.matrix4f = matrix4f;
+        }
+
+        @Override
+        public VertexConsumer getBuffer(RenderType type) {
+            RenderType localType = type;
+            if (localType instanceof RenderType.CompositeRenderType) {
+                // all of this requires a lot of AT's so be aware of that on ports
+                ResourceLocation texture = ((RenderStateShard.TextureStateShard) ((RenderType.CompositeRenderType) localType).state.textureState).texture
+                        .orElse(InventoryMenu.BLOCK_ATLAS);
+
+                localType = entityTranslucentCull(texture);
+            } else if (localType.toString().equals(Sheets.translucentCullBlockSheet().toString())) {
+                localType = Sheets.translucentCullBlockSheet();
+            }
+
+            return new DireVertexConsumerSquished(this.inner.getBuffer(localType), 0, 0, 0, 1, squishAmt, 1, matrix4f);
+        }
+    }
+
+    public static void renderCopy(PoseStack matrix, BlockPos startPos, BlockPos endPos, Color color) {
+        if (startPos == null || endPos == null)
+            return;
+
+        //We want to draw from the starting position to the (ending position)+1
+        int x = Math.min(startPos.getX(), endPos.getX()), y = Math.min(startPos.getY(), endPos.getY()), z = Math.min(startPos.getZ(), endPos.getZ());
+
+        int dx = (startPos.getX() > endPos.getX()) ? startPos.getX() + 1 : endPos.getX() + 1;
+        int dy = (startPos.getY() > endPos.getY()) ? startPos.getY() + 1 : endPos.getY() + 1;
+        int dz = (startPos.getZ() > endPos.getZ()) ? startPos.getZ() + 1 : endPos.getZ() + 1;
+
+        MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+        VertexConsumer builder = buffer.getBuffer(DireRenderTypes.lines());
+
+        matrix.pushPose();
+        Matrix4f matrix4f = matrix.last().pose();
+        Matrix3f matrix3f = matrix.last().normal();
+        int colorRGB = color.getRGB();
+
+        builder.vertex(matrix4f, x, y, z).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, z).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, y, z).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, z).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, y, z).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, x, y, dz).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, z).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, z).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, z).color(colorRGB).normal(matrix3f, -1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, z).color(colorRGB).normal(matrix3f, -1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, z).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, dz).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, dz).color(colorRGB).normal(matrix3f, 0.0F, -1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, y, dz).color(colorRGB).normal(matrix3f, 0.0F, -1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, x, y, dz).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, dz).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, dz).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, -1.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, z).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, -1.0F).endVertex();
+        builder.vertex(matrix4f, x, dy, dz).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, dz).color(colorRGB).normal(matrix3f, 1.0F, 0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, y, dz).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, dz).color(colorRGB).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, z).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, dx, dy, dz).color(colorRGB).normal(matrix3f, 0.0F, 0.0F, 1.0F).endVertex();
+
+        buffer.endBatch(DireRenderTypes.lines()); // @mcp: draw = finish
+        matrix.popPose();
+    }
+
+    public static void renderBoxSolid(Matrix4f matrix, VertexConsumer builder, BlockPos pos, float r, float g, float b, float alpha) {
+        double x = pos.getX() - 0.001;
+        double y = pos.getY() - 0.001;
+        double z = pos.getZ() - 0.001;
+        double xEnd = pos.getX() + 1.0015;
+        double yEnd = pos.getY() + 1.0015;
+        double zEnd = pos.getZ() + 1.0015;
+
+        renderBoxSolid(matrix, builder, x, y, z, xEnd, yEnd, zEnd, r, g, b, alpha);
+    }
+
+    protected static void renderBoxSolid(Matrix4f matrix, VertexConsumer builder, double x, double y, double z, double xEnd, double yEnd, double zEnd, float red, float green, float blue, float alpha) {
+        //careful: mc want's it's vertices to be defined CCW - if you do it the other way around weird cullling issues will arise
+        //CCW herby counts as if you were looking at it from the outside
+        float startX = (float) x;
+        float startY = (float) y;
+        float startZ = (float) z;
+        float endX = (float) xEnd;
+        float endY = (float) yEnd;
+        float endZ = (float) zEnd;
+
+        //down
+        builder.vertex(matrix, startX, startY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, startY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, startY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, startY, endZ).color(red, green, blue, alpha).endVertex();
+
+        //up
+        builder.vertex(matrix, startX, endY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, endY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, startZ).color(red, green, blue, alpha).endVertex();
+
+        //east
+        builder.vertex(matrix, startX, startY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, endY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, startY, startZ).color(red, green, blue, alpha).endVertex();
+
+        //west
+        builder.vertex(matrix, startX, startY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, startY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, endY, endZ).color(red, green, blue, alpha).endVertex();
+
+        //south
+        builder.vertex(matrix, endX, startY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, endY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, endX, startY, endZ).color(red, green, blue, alpha).endVertex();
+
+        //north
+        builder.vertex(matrix, startX, startY, startZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, startY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, endY, endZ).color(red, green, blue, alpha).endVertex();
+        builder.vertex(matrix, startX, endY, startZ).color(red, green, blue, alpha).endVertex();
+    }
+}
---
+++ common/src/main/java/com/hollingsworth/schematic/client/renderer/DireRenderTypes.java
@@ -0,0 +1,105 @@
+package com.hollingsworth.schematic.client.renderer;
+
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
+
+public class DireRenderTypes extends RenderType {
+    public static final RenderType RenderBlock = create("GadgetRenderBlock",
+            DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
+            RenderType.CompositeState.builder()
+//                    .setShadeModelState(SMOOTH_SHADE)
+                    .setShaderState(RenderStateShard.RENDERTYPE_SOLID_SHADER)
+                    .setLightmapState(LIGHTMAP)
+                    .setTextureState(BLOCK_SHEET_MIPPED)
+                    .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                    .setDepthTestState(LEQUAL_DEPTH_TEST)
+                    .setCullState(CULL)
+                    .setWriteMaskState(COLOR_WRITE)
+                    .createCompositeState(false));
+    public static final RenderType RenderBlockFade = create("GadgetRenderBlockFade",
+            DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
+            RenderType.CompositeState.builder()
+//                    .setShadeModelState(SMOOTH_SHADE)
+                    .setShaderState(RenderStateShard.RENDERTYPE_SOLID_SHADER)
+                    .setLightmapState(LIGHTMAP)
+                    .setTextureState(BLOCK_SHEET_MIPPED)
+                    .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                    .setDepthTestState(LEQUAL_DEPTH_TEST)
+                    .setCullState(CULL)
+                    .setWriteMaskState(COLOR_DEPTH_WRITE)
+                    .createCompositeState(false));
+    public static final RenderType RenderBlockBackface = create("GadgetRenderBlockBackface",
+            DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
+            RenderType.CompositeState.builder()
+//                    .setShadeModelState(SMOOTH_SHADE)
+                    .setShaderState(RenderStateShard.RENDERTYPE_SOLID_SHADER)
+                    .setLightmapState(LIGHTMAP)
+                    .setTextureState(BLOCK_SHEET_MIPPED)
+                    .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                    .setDepthTestState(LEQUAL_DEPTH_TEST)
+                    .setCullState(NO_CULL)
+                    .setWriteMaskState(COLOR_DEPTH_WRITE)
+                    .createCompositeState(false));
+
+    public static final RenderType RenderBlockFadeNoCull = create("GadgetRenderBlockFadeNoCull",
+            DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
+            RenderType.CompositeState.builder()
+//                    .setShadeModelState(SMOOTH_SHADE)
+                    .setShaderState(RenderStateShard.RENDERTYPE_SOLID_SHADER)
+                    .setLightmapState(LIGHTMAP)
+                    .setTextureState(BLOCK_SHEET_MIPPED)
+                    .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                    .setDepthTestState(LEQUAL_DEPTH_TEST)
+                    .setCullState(CULL)
+                    .setWriteMaskState(COLOR_WRITE)
+                    .createCompositeState(false));
+
+    public static final RenderType TRIANGLE_STRIP =
+            create("triangle_strip", DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_STRIP, 256, false, false,
+                    RenderType.CompositeState.builder()
+                            .setShaderState(RenderStateShard.POSITION_COLOR_SHADER)
+                            .setTextureState(NO_TEXTURE)
+                            .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                            .setCullState(NO_CULL)
+                            .setLightmapState(NO_LIGHTMAP)
+                            .createCompositeState(false));
+
+    public static final RenderType MissingBlockOverlay = create("GadgetMissingBlockOverlay",
+            DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, 256, false, false,
+            RenderType.CompositeState.builder()
+                    .setShaderState(RenderStateShard.POSITION_COLOR_SHADER)
+                    .setLayeringState(VIEW_OFFSET_Z_LAYERING) // view_offset_z_layering
+                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                    .setTextureState(NO_TEXTURE)
+                    .setDepthTestState(LEQUAL_DEPTH_TEST)
+                    .setCullState(NO_CULL)
+                    .setLightmapState(NO_LIGHTMAP)
+                    .setWriteMaskState(COLOR_WRITE)
+                    .createCompositeState(false));
+
+    /*public static void updateRenders() {
+        RenderBlockBackface = create("GadgetRenderBlockBackface",
+                DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
+                RenderType.CompositeState.builder()
+//                    .setShadeModelState(SMOOTH_SHADE)
+                        .setShaderState(RenderStateShard.RENDERTYPE_SOLID_SHADER)
+                        .setLightmapState(LIGHTMAP)
+                        .setTextureState(BLOCK_SHEET_MIPPED)
+                        .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .setDepthTestState(LEQUAL_DEPTH_TEST)
+                        .setCullState(NO_CULL)
+                        .setWriteMaskState(COLOR_DEPTH_WRITE)
+                        .createCompositeState(false));
+    }*/
+
+    public DireRenderTypes(String p_173178_, VertexFormat p_173179_, VertexFormat.Mode p_173180_, int p_173181_, boolean p_173182_, boolean p_173183_, Runnable p_173184_, Runnable p_173185_) {
+        super(p_173178_, p_173179_, p_173180_, p_173181_, p_173182_, p_173183_, p_173184_, p_173185_);
+    }
+}
