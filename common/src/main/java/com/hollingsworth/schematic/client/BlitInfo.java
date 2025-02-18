903c68e3993773f1aba475dd58ccf42371d8c739
Bailey Hollingsworth <baileyholl@gmail.com>
2025-02-17 19:21:36 -0600

    Delete BlitInfo.java

--- common/src/main/java/com/hollingsworth/schematic/client/BlitInfo.java
+++
@@ -1,9 +0,0 @@
-package com.hollingsworth.schematic.client;
-
-import net.minecraft.resources.ResourceLocation;
-
-public record BlitInfo(ResourceLocation location, int u, int v, int width, int height) {
-    public BlitInfo(ResourceLocation location, int width, int height){
-        this(location, 0, 0, width, height);
-    }
-}
