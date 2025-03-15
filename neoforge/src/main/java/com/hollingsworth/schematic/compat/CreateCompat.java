ed3c6f0a028e69436bfb80d69a0b090a2f2fb81e
Bailey Hollingsworth <baileyholl@gmail.com>
2025-03-15 17:22:23 -0500

    Reenable create compat

--- neoforge/src/main/java/com/hollingsworth/schematic/compat/CreateCompat.java
+++ neoforge/src/main/java/com/hollingsworth/schematic/compat/CreateCompat.java
@@ -1,5 +1,6 @@
 package com.hollingsworth.schematic.compat;
 
+import com.simibubi.create.content.schematics.SchematicAndQuillItem;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
@@ -7,6 +8,6 @@ import net.minecraft.world.phys.AABB;
 public class CreateCompat {
 
     public static void appendGlue(Level level, AABB aabb, CompoundTag tag){
-//        SchematicAndQuillItem.clampGlueBoxes(level, aabb, tag);
+        SchematicAndQuillItem.clampGlueBoxes(level, aabb, tag);
     }
 }
