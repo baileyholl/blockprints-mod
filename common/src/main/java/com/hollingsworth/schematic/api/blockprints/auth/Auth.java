17661407d6bcb14897516e1cc9cb9b24cbc29e68
Bailey Hollingsworth <baileyholl@gmail.com>
2025-04-11 11:01:28 -0500

    Fix enter code screen

--- common/src/main/java/com/hollingsworth/schematic/api/blockprints/auth/Auth.java
+++ common/src/main/java/com/hollingsworth/schematic/api/blockprints/auth/Auth.java
@@ -2,20 +2,15 @@ package com.hollingsworth.schematic.api.blockprints.auth;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.hollingsworth.schematic.api.blockprints.ApiError;
 import com.hollingsworth.schematic.api.blockprints.ApiResponse;
 import com.hollingsworth.schematic.api.blockprints.BlockprintsApi;
 import com.hollingsworth.schematic.api.blockprints.RequestUtil;
 import com.hollingsworth.schematic.oauth.Login;
 import net.minecraft.client.Minecraft;
-import org.apache.http.client.utils.URIBuilder;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 
 public class Auth {
     private final BlockprintsApi api;
@@ -24,33 +19,6 @@ public class Auth {
     public Auth(BlockprintsApi api) {
         this.api = api;
         this.CLIENT = this.api.CLIENT;
-    }
-
-    public ApiResponse<BlockprintsToken> getToken() throws ApiError{
-        try{
-            URIBuilder uriBuilder = new URIBuilder(RequestUtil.getDomain() + "/api/v1/auth");
-            HttpRequest.Builder reqBuilder = null;
-            if (Minecraft.getInstance().getUser().getName().equals("Dev") || Minecraft.getInstance().getUser().getAccessToken().equals("FabricMC")) {
-                reqBuilder = HttpRequest.newBuilder();
-            }else{
-                reqBuilder = HttpRequest.newBuilder()
-                        .header("authorization", "Bearer " + Minecraft.getInstance().getUser().getAccessToken());
-            }
-            HttpRequest request = reqBuilder.header("Content-Type", "application/json")
-                    .uri(uriBuilder.build())
-                    .timeout(Duration.ofSeconds(8))
-                    .GET().build();
-            var res = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            var code = res.statusCode();
-            if (!RequestUtil.responseSuccessful(code)) {
-                throw new Error("Failed to authenticate with Blockprints API");
-            }
-            JsonObject responseObj = JsonParser.parseString(res.body()).getAsJsonObject();
-            return ApiResponse.success(new BlockprintsToken(responseObj.get("token").getAsString(), responseObj.get("expiresAt").getAsInt(), Minecraft.getInstance().player.getUUID()));
-        }catch (URISyntaxException | IOException | InterruptedException | Error e) {
-            e.printStackTrace();
-            throw new ApiError("Failed to authenticate with Blockprints API. Are you logged in to Minecraft?");
-        }
     }
 
     public ApiResponse<BlockprintsToken> postMSCode(String code) {
